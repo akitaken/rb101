40169a06c0c6872ac4129832ce07aa4f173e66eb
Cathy R <cat@catr.co>
2020-05-07 18:56:36 +1000

    Added/updated Lesson 2 exercises

---
+++ lesson2/debugging.rb
@@ -0,0 +1,21 @@
+def car(new_car)
+  make = make(new_car)
+  model = model(new_car)
+  [make, model]
+end
+
+def make(new_car)
+  new_car.split(" ")[0]
+end
+
+def model(new_car)
+  new_car.split(" ")[1] 
+end
+
+make, model = car("Ford Mustang")
+puts make == "Ford"         # => true
+puts model.start_with?("M")
+
+
+# puts model.start_with?("M") rescue false # => this will supress the error but is BAD - try to fix the error instead.
+
