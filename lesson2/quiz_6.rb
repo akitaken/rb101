3b0185e35ce1e8223eaa2a47b956db4623ee5193
akitaken <cat@catr.co>
2020-05-11 22:27:23 +1000

    Adding practice exercises

--- lesson2/quiz_6.rb
+++
@@ -1,6 +0,0 @@
-num = 8
-
-if num >= 8 && num < 6 || num > 4
-  puts 'Goodbye'
-end
-
---
+++ lesson2/quizzing.rb
@@ -0,0 +1,14 @@
+# exercises for quiz
+
+
+def name1
+  "George"
+end
+
+name = "Lisa"
+
+def display_name
+  puts name
+end
+
+display_name
